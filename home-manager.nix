728874294178552d65d5c2063168b1d4ac47f19a
Robert Humphrey <humphreyrp92@gmail.com>
2025-07-11 08:28:15 -0700

    Remove comment lines

--- home-manager.nix
+++ home-manager.nix
@@ -6,21 +6,6 @@ in
   home.username = "robbie";
   home.homeDirectory = "/home/robbie";
 
-  # link the configuration file in current directory to the specified location in home directory
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # link all files in `./scripts` to `~/.config/i3/scripts`
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
   # set cursor size and dpi for 4k monitor
   xresources.properties = {
     "Xcursor.size" = 16;
