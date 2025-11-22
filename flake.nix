774f6dfb2663d0fe8a35e37b2dd0ad1335c3109f
humphreyrp92 <humphreyrp92@gmail.com>
2025-11-22 07:23:47 -0800

    Cleanups

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "My NixOS base configuration";
-  inputs = rec {
+  inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     homeManager = {
       url = "github:nix-community/home-manager";
@@ -23,6 +23,7 @@
       nixosConfigurations = {
         machines = import ./machines { inherit nixpkgs homeManager common; };
       };
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+      # TODO: Make common for all systems
+      formatter.x86_64-linux = pkgs.nixfmt-rfc-style;
     };
 }
