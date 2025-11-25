b6588fe688febaec12b5d6392cbc8dbf30c95b9d
humphreyrp92 <humphreyrp92@gmail.com>
2025-11-24 16:15:19 -0800

    Minor cleanups

--- machines/storage/samba.nix
+++ machines/storage/samba.nix
@@ -28,21 +28,18 @@
     openFirewall = true;
     settings = {
       global = {
-        "workgroup" = "WORKGROUP";
+        workgroup = "WORKGROUP";
         "server string" = "smbnix";
         "netbios name" = "smbnix";
-        "security" = "user";
-        #"use sendfile" = "yes";
-        #"max protocol" = "smb2";
-        # note: localhost is the ipv6 localhost ::1
+        security = "user";
         "hosts allow" = "192.168.1. 127.0.0.1 localhost";
         "hosts deny" = "0.0.0.0/0";
         "guest account" = "nobody";
         "map to guest" = "bad user";
       };
       "public" = {
-        "path" = "/data/public";
-        "browseable" = "yes";
+        path = "/data/public";
+        browseable = "yes";
         "read only" = "no";
         "guest ok" = "yes";
         "create mask" = "0644";
