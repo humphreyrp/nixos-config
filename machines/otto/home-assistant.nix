578086518a44f398d1d6be9362155395fc9387ab
humphreyrp92 <humphreyrp92@gmail.com>
2025-12-10 12:52:03 -0800

    Use home assistant ui automation configurations for now

--- machines/otto/home-assistant.nix
+++ machines/otto/home-assistant.nix
@@ -18,6 +18,9 @@
       # I'm disabling the default configuration, because it comes with a bunch of cruft like
       # automatic device discovery which will throw ModuleNotFound errors for any device type that
       # you have on your network that you don't have drivers for in home assistant.
+
+      # For now automations are done via the ui
+      automation = "!include automations.yaml";
     };
   };
 }
