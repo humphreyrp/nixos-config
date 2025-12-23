2520dd3e1c6f75543d5377a92d6ca72dd7763d96
humphreyrp92 <humphreyrp92@gmail.com>
2025-12-23 09:37:28 -0800

    Add initial Caddy VM

--- machines/machines.nix
+++ machines/machines.nix
@@ -8,4 +8,5 @@
   otto = import ./otto/otto.nix { inherit nixpkgs homeManager common; };
   dev = import ./dev/dev.nix { inherit nixpkgs homeManager common; };
   backupServer = import ./backup-server/backup-server.nix { inherit nixpkgs homeManager common; };
+  revProxy = import ./rev-proxy/rev-proxy.nix { inherit nixpkgs homeManager common; };
 }
