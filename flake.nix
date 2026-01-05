be279335875dc4f93c50210038185e2d7fb322e6
humphreyrp92 <humphreyrp92@gmail.com>
2026-01-04 20:12:14 -0800

    Add formatter

--- flake.nix
+++ flake.nix
@@ -16,6 +16,8 @@
     let
       commonConfig = { pkgs, ...}: import ./common/common.nix { inherit pkgs; };
       systems = [ "x86_64-linux" "aarch64-linux" ];
+
+      # Generate an output package set for all supported systems
       packages = nixpkgs.lib.genAttrs systems (system:
         let
           pkgs = import nixpkgs { inherit system; };
@@ -24,6 +26,12 @@
             inherit pkgs;
             common = commonConfig { inherit pkgs; };
           });
+
+      # Formatter for each system
+      formatter = nixpkgs.lib.genAttrs systems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in pkgs.nixfmt-rfc-style
+      );
       machines = import ./machines/machines.nix {
         inherit nixpkgs homeManager;
         common = packages."x86_64-linux".common;
@@ -32,6 +40,7 @@
     {
       nixosConfigurations = machines;
       packages = packages;
-      # formatter.x86_64-linux = pkgs.nixfmt-rfc-style;
+      formatter = formatter;
+     # formatter.x86_64-linux = pkgs.nixfmt-rfc-style;
     };
 }
