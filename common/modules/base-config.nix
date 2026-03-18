8d7f158bce1efee32930c679a1554b73f6c057e4
Robert Humphrey <humphreyrp92@gmail.com>
2026-03-18 15:01:12 -0700

    Add note

--- common/modules/base-config.nix
+++ common/modules/base-config.nix
@@ -62,6 +62,15 @@
   '';
 
   # Run periodic garbage collection
+  # Note: I'm always in a fight to clear space in the nix store. A trick that I've found is to
+  # make sure I'm deleting my development build products that might hang around my file system. To
+  # do that, I'll run `find . -type l -xtype d -name result -delete` periodically, which deletes
+  # any result symlinks.
+  # Also, it helps to sort the root filesystem to look for particularly bulk files and directories
+  # with `sudo du -h / | sort -rh | head -n 20`, then if there are any suspicious nix closures,
+  # check the gc root causing them to hang around with `nix-store --query --roots <store-path>.
+  # Also, nix will store caches of data under `~/.cache` which can be bulky. Helps to clear those
+  # out periodically.
   nix.gc.automatic = true;
   nix.gc.dates = "weekly";
 
