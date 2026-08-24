5833fc73206cb700bb737958ef3c83f78fa6f5e5
Robert Humphrey <humphreyrp92@gmail.com>
2026-08-23 20:57:18 -0700

    Bump

--- common/home-manager/home-manager.nix
+++ common/home-manager/home-manager.nix
@@ -17,7 +17,6 @@
 
   # Packages that should be installed to the user profile.
   home.packages = [
-    pkgs.fzf
     pkgs.ripgrep
     pkgs.virtualenv
   ];
@@ -83,7 +82,7 @@
   # You can update home Manager without changing this value. See
   # the home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "24.11";
+  home.stateVersion = "26.05";
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
