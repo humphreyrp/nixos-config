5557d467bb738f44da8d1c28d52325d814dff5e3
Robert Humphrey <humphreyrp92@gmail.com>
2026-08-23 21:21:50 -0700

    Add back in nvim

--- common/home-manager/modules/nvim.nix
+++ common/home-manager/modules/nvim.nix
@@ -4,33 +4,34 @@
     enable = true;
   };
 
-  # programs.neovim = {
-  #   enable = true;
-  #   defaultEditor = true;
-  #   viAlias = true;
-  #   vimAlias = true;
-  #   plugins = with pkgs.vimPlugins; [
-  #     nvim-lspconfig
-  #     vscode-nvim
-  #     telescope-nvim
-  #     telescope-file-browser-nvim
-  #     haskell-vim
-  #     auto-session
-  #     nvim-cmp
-  #     cmp-nvim-lsp
-  #     vim-gitgutter
-  #     vim-better-whitespace
-  #     comment-nvim
-  #     blamer-nvim
-  #     nvim-web-devicons
-  #     vim-sleuth
-  #     vim-surround
-  #     nvim-scrollbar
-  #     rustaceanvim
-  #     lualine-nvim
-  #     render-markdown-nvim
-  #     diffview-nvim
-  #   ];
-  # };
-  home.file.".config/nvim/init.lua".source = ./nvim/init.lua;
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      nvim-lspconfig
+      vscode-nvim
+      telescope-nvim
+      telescope-file-browser-nvim
+      haskell-vim
+      auto-session
+      nvim-cmp
+      cmp-nvim-lsp
+      vim-gitgutter
+      vim-better-whitespace
+      comment-nvim
+      blamer-nvim
+      nvim-web-devicons
+      vim-sleuth
+      vim-surround
+      nvim-scrollbar
+      rustaceanvim
+      lualine-nvim
+      render-markdown-nvim
+      diffview-nvim
+    ];
+    initLua = ./nvim/init.lua;
+  };
+  # home.file.".config/nvim/init.lua".source = ./nvim/init.lua;
 }
